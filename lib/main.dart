637ea960c99366a7bb96e39067872a2b2bcd0725
subodh-rakshit <r2609rakshit@gmail.com>
2023-03-25 13:16:56 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  int days = 30;
+  String name = "Subodh Rakshit";
+  // double pi = 3.14;
+  // bool isMale = true;
+  // num temp = 30.5;  // This can take double and int also
+  // var day = "Sunday"; // Var can take any kind of datatype
+  // var month = 9;
+  // static const piValue = 3.14;
+
+  // final nameOfPerson = "Subodh";
+
+  // Difference between final and const is that if we have a list with final and const then we cannot change the list with the const keyword but we can change the value which is with the final keyword.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
